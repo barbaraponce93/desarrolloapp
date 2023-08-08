47ca1a2fb009afe215d792974e18b0ad3750c1f9
Chazzero <chari230893@gmail.com>
2023-08-08 10:53:09 -0300

    clase3f

---
+++ src/desarrolloapp/Clase3F.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desarrolloapp;
+
+/**
+ *
+ * @author chari
+ */
+public class Clase3F {
+    private String nom;
+    private String ape;
+    private int id;
+    
+    
+    
+}
